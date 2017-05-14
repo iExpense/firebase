7fe125d4249892c7550112e7f96c6b86569b1c01
Aman Mangal <mangalaman93@gmail.com>
2017-05-14 18:55:27 +0530

    fix compilation issue
    when another package firebase, the static libraries are
    found missing. This issue is fixed by specifying absolute
     path instead of relative path using the var ${SRCDIR}

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 package firebase
 
 // #cgo CXXFLAGS: -D_GLIBCXX_USE_CXX11_ABI=0 -Wabi-tag  -std=c++11
-// #cgo LDFLAGS: -D_GLIBCXX_USE_CXX11_ABI=0 -Wabi-tag -Lcpplib -ldatabase -lauth -lapp
+// #cgo LDFLAGS: -D_GLIBCXX_USE_CXX11_ABI=0 -Wabi-tag -L${SRCDIR}/cpplib -ldatabase -lauth -lapp
 import "C"
